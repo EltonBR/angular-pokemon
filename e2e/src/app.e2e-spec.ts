3c633cf67c97b33b43b2d9aae66198a42585b271
EltonEJ <eltonej4598@gmail.com>
2020-08-01 01:40:29 -0300

    written simple test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,47 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-pokemon app is running!');
+    expect(page.getTitleText()).toEqual('Pokemons');
+  });
+
+  it('click first card', () => {
+    page.clickFistCard();
+  });
+
+  it('return to home', () => {
+    page.clickButtonHomeInCardDetails();
+   });
+
+  it('search pikachu', () => {
+    page.searchCard("pika");
+  });
+
+  it('search empty', () => {
+    page.searchCard("");
+  });
+
+  it('search bello', () => {
+    page.searchCard("bello");
+  });
+
+  it('search empty', () => {
+    page.searchCard("");
+  });
+
+  it('search pikachu', () => {
+    page.searchCard("pika");
+  });
+
+  it('click first card', () => {
+    setTimeout(() => page.clickFistCard(), 2000);
+  });
+
+  it('return to home again', () => {
+    page.clickButtonHomeInCardDetails();
+  });
+
+  it('scroll to end of page', () => {
+    page.scrollToEnd();
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor, Locator } from 'protractor';
 
 export class AppPage {
+
+  waitElement(elementSelector: Locator) {
+    var until = protractor.ExpectedConditions;
+    let elem = element(elementSelector);
+    browser.wait(until.presenceOf(elem), 20000, 'Element taking too long to appear in the DOM');
+    return elem;
+  }
+
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('.header-container .header h1')).getText() as Promise<string>;
+  }
+
+  clickFistCard(): void {
+    console.log(this.waitElement(by.className('flex-grid-item')))
+    this.waitElement(by.className('flex-grid-item')).click();
+  }
+
+  clickButtonHomeInCardDetails(): void {
+    this.waitElement(by.id('home-btn')).click();
+  }
+
+  scrollToEnd(): void {
+    browser.executeScript('document.querySelector(\'html\').scroll(0, 9000);');
+  }
+
+  searchCard(data: string): void {
+    browser.findElement(by.id('search-input')).then((element) => {
+      if (data.length === 0) {
+        element.clear();
+      } else {
+        element.sendKeys(data);
+      }
+    });
   }
 }
